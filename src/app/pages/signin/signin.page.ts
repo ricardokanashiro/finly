f94f683f417ff4542ded1917637b814154067aa3
Ricardo <ricardo.kanashiro.adm@gmail.com>
2025-02-22 15:38:08 -0300

    fix(base pages): fix standalone setting as false on pages home, signin and login

--- src/app/pages/signin/signin.page.ts
+++ src/app/pages/signin/signin.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-signin',
   templateUrl: './signin.page.html',
   styleUrls: ['./signin.page.scss'],
+  standalone: false
 })
 export class SigninPage implements OnInit {
 
